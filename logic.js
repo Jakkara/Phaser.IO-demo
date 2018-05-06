4b9c5c3133327c7210eb0dce179b82ef0058a3d5
Jali Rainio <jali.rainio@gmail.com>
2018-05-06 14:58:58 +0300

    Page 4

--- logic.js
+++ logic.js
@@ -1,18 +1,37 @@
 var game = new Phaser.Game(800, 800, Phaser.CANVAS, 'phaser-example', { create: create, preload: preload});
 
 var page1 = [
-    "Olet löytänyt pienimuotoisen demonstraation, joka on toteutettu ",
-    "Tämän sivuston tarkoitus ei ole olla parhaita käytäntöjä noudattava mestariteos, jolla hankitaan työhaastattelu ja miljoonia dollareita crowdfundingia.\n",
-    "Tämän ei myöskään ole tarkoitus esitellä Phaser-frameworkin useita monipuolisia ominaisuuksia, joita asiaan vihkiytyneet osaisivat varmasti arvostaa.\n",
+    "Olet löytänyt pienimuotoisen demonstraation, joka on toteutettu Phaser-frameworkilla.","\n",
+    "Tämä sivusto ei ole parhaita käytäntöjä noudattava mestariteos, jolla hankitaan työhaastattelu ja miljoonia dollareita crowdfundingia.\n",
+    "Tämä ei myöskään ole esittely frameworkin moninaisista ominaisuuksista, joita asiaan vihkiytyneet osaisivat varmasti arvostaa.",
     "\n",
-    "Päinvastoin.",
-    "\n",
-    
+    "Päinvastoin."
 ];
 var page2 = [
-    "Tämän sivuston on tarkoitus on esitellä, miltä kahden iltapäivän perehtyminen täysin vieraaseen ympäristöön näyttää.",
+    "Kun alat opettelemaan uutta asiaa, on hyvä lähteä pohjatiedoista.\n",
+    "Kauneinkaan talo ei pysy pystyssä jos sen perusteet on pystytetty saveen. \nVankka pohja on ehdoton vaatimus kaikelle oppimiselle.",
     "\n",
+    "Mutta toisaalta, mikään ei myöskään estä puuseppää rakentamasta taloa vaikka hän ei osaisi vasaaraakaan heiluttaa.\n",
+    "Tämä sivusto on esimerkki puusepästä, joka päätti rakentaa talon ilman vasaraa.",
+    "\n",
+];
+
+var page3 = [
+    "Tämän demonstraation kehitys lähti melko tyhjältä pohjalta.\n",
+    "Ainoa käytetty ohjeistus oli Phaserin asennusohje. Kaikki muu kasattiin esimerkkien pohjalta, trial-and-error -asenteella.\n",
+    "Jokainen ruudulla oleva elementti on ollut pienimuotoisen kiroilun ja reilun ihmettelyn takana.\n",
+    "Lähdekoodi muistuttaakin melkoisesti korttitaloa, joka on askeleen päässä räjähtämästä käsiin.\n",
+    "\n",
+    "Haluankin nyt jakaa muutamia kohtaamiani ongelmia."
 ]
+
+var page4 = [
+    "Tekstin kirjoittaminen ja pyyhkiminen ruudulta on toteutettu toistamalla substring-funktiota millisekunttien välein.\n",
+    "Tämä on kuitenkin asynkroninen metodi, joka pyörii irrallisena muun ohjelman toiminnasta.\n",
+    "Sivujen välillä onkin siis asetettu manuaalisesti oikea määrä viivettä, ettei seuraavan sivun kirjoittaminen ala ennen aiemman pyyhkimistä.\n",
+    "Näkemäsi viive.",".",".", " on purukumiviritelty sekoitus Phaserin toisto-ominaisuutta, tapahtumien viivyttämistä ja JavaScriptin setTimeout-funktion luovaa käyttöä."
+]
+
 
 //Manages font loading from Google Fonts. 1 second delay to load all fonts safely.
 WebFontConfig = {
@@ -23,7 +42,7 @@ WebFontConfig = {
 
 };
 
-var pages = [page1,page2];
+var pages = [page1,page2, page3, page4];
 var pageNumber = 0;
 var currentPage = pages[pageNumber];
 
@@ -37,8 +56,8 @@ var music;
 var line = [];
 var wordIndex = 0;
 var lineIndex = 0;
-var wordDelay = 1;
-var lineDelay = 1200;
+var wordDelay = 20;
+var lineDelay = 1400;
 var temp = false;
 
 function create() {
@@ -59,12 +78,11 @@ function createText(){
     setTimeout(function(){
         nextLine()
     },5000);
-    //game.time.events.add(5000, nextLine, this);
 }
 
 function introScreen(){ 
     displayText.fontSize = 60;
-    displayText.setText('Ladataan \ndramaattista \nretrofiilistelyä...');
+    displayText.setText('Ladataan...');
     setTimeout(function(){
         displayText.setText('Tervetuloa.\n')
     }, 2500);
@@ -72,16 +90,18 @@ function introScreen(){
 
 //reads the next line of text
 function nextLine(){
+    //reset the font size at the beginning of the page
     if (lineIndex == 0){
         displayText.fontSize = 26;
         displayText.setText('');
     }
+    // Have we reached the end of the page?
     if (lineIndex === currentPage.length){
         //  We're finished. Clean the display and turn the page.
         backspaceTransition()
         setTimeout(function(){
             turnThePage();
-        },3000);
+        },4000);
         return;
         }
     //  Split the current line into an array
@@ -103,7 +123,7 @@ function nextCharacter(){
     //  end of the line
     if (wordIndex === line.length){
         // Insert newline
-        displayText.text = displayText.text.concat("\n");
+        //displayText.text = displayText.text.concat("\n");
         // Proceed to next line
         game.time.events.add(lineDelay, nextLine, this);
     }
@@ -121,8 +141,10 @@ function backspace(){
 function turnThePage(){
     //at the last page already, don't turn the page
     if (pageNumber == pages.length-1){
+        music.stop();
         return;
     }
+    //clear the indexes for the next page
     lineIndex = 0;
     wordIndex = 0;
     pageNumber++;
